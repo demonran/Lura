a55cc890eef4487b75c02c0e5adbd973450de8c5
Liu Ran <liuran@tcl.com>
2017-02-26 22:37:20 +0800

    static pages add navigation

--- ui/src/main/resources/static/js/app.js
+++ ui/src/main/resources/static/js/app.js
@@ -6,11 +6,26 @@ var app = angular.module('myApp', ['ngRoute']);
 app.config(['$routeProvider',function($routeProvider) {
     $routeProvider
         .when('/',{
+            templateUrl:"/pages/welcome.html",
+        })
+        .when('/sf',{
             templateUrl: "/pages/tables.html",
             controller:'tables'
             })
+        .when('/wx',{
+            //templateUrl: "/pages/tables.html",
+            //controller:'tables'
+        })
+
         .otherwise({redirectTo: '/'});
 
 
     }]);
+app.run(['$rootScope', '$location', function($rootScope, $location) {
 
+    $rootScope.$on('$routeChangeSuccess', function() {
+        $rootScope.path = $location.path();
+      //  console.log($location.path());
+    });
+
+}]);
